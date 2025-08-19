436d1f9acdd12ec437cb50b9c127446e953aa655
XuGuangnian <gengen.wang@outlook.com>
2025-08-19 15:43:00 +0800

    更新convert.sh

--- convert.sh
+++ convert.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+
+# 转换milvus chart 为milvus-5.0.0 chart
+
 # 错误处理
 set -e
 
@@ -13,13 +16,10 @@ sed -i 's/^\(\s*repository:\) "\([^"]*\)"/\1 \2/' $WORK_DIR/ds-milvus-values.yam
 # namespace: default 改为 namespace: milvus
 sed -i 's/^\(\s*namespace:\) default/\1 milvus/' $WORK_DIR/ds-milvus-values.yaml
 
-# registry: docker.io 改为 registry: REGIP
-find $WORK_DIR/ -name "*.yaml" -type f | xargs sed -i 's/^\(\s*registry:\) docker.io/\1 REGIP/'
+# registry: docker.io 改为 registry:
+find $WORK_DIR/ -name "*.yaml" -type f | xargs sed -i 's/^\(\s*registry:\) docker.io/\1/'
 
 # repository：xx 改为 repository：REGIP/xx
 sed -i 's/^\(\s*repository:\) \([^"]*\)/\1 REGIP\/\2/' $WORK_DIR/ds-milvus-values.yaml
 
 echo "转换完成"
-
-
-
