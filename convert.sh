5db4757b1f8ee1f25b8831044e6aa410413116ef
XuGuangnian <gengen.wang@outlook.com>
2025-08-19 11:42:33 +0800

    更新convert

--- convert.sh
+++ convert.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
+# 错误处理
+set -e
 
-WORK_DIR=/root/addons/milvus-5.0.0
+# WORK_DIR=/root/addons/milvus-5.0.0
+WORK_DIR=/home/gengen/workspace/helm/milvus
 
 cp $WORK_DIR/values.yaml $WORK_DIR/ds-milvus-values.yaml
 
@@ -14,8 +17,9 @@ sed -i 's/^\(\s*namespace:\) default/\1 milvus/' $WORK_DIR/ds-milvus-values.yaml
 find $WORK_DIR/ -name "*.yaml" -type f | xargs sed -i 's/^\(\s*registry:\) docker.io/\1 REGIP/'
 
 # repository：xx 改为 repository：REGIP/xx
-find $WORK_DIR/ -name "*.yaml" -type f | xargs sed -i 's/^\(\s*repository:\) \([^"]*\)/\1 REGIP\/\2/'
+sed -i 's/^\(\s*repository:\) \([^"]*\)/\1 REGIP\/\2/' $WORK_DIR/ds-milvus-values.yaml
 
+echo "转换完成"
 
 
 
